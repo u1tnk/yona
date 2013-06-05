33f221135e42b6b0bfbb101a266d98c6204735e7
yuichi tanaka <u1tnk1123@gmail.com>
2013-06-05 11:24:16 +0900

    cookieが保存されず、ログイン不可になっていたので一旦cache_storeへ

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Yona::Application.config.session_store :cookie_store, key: '_yona_session'
+# cookieがexpire=現在時間で発行されるので一旦cache_storeに変更
+# Yona::Application.config.session_store :file_store, key: '_yona_session'
+Yona::Application.config.session_store :cache_store, key: '_yona_session'
